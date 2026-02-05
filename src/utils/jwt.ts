32c479584f3d114775895e4c95a512f84ec53d3b
khumanpoudel <khumanpoudel124421@gmail.com>
2026-02-05 15:51:42 +0000

    jwt util to sign tokens

---
+++ src/utils/jwt.ts
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken"
+
+const secret:string = process.env.JWT_SECRET as string
+
+export const Sign = (
+    payload:object,
+    expire:jwt.SignOptions={expiresIn: '1h'}
+) => jwt.sign(payload,secret,expire);
+
+
+
+
+
