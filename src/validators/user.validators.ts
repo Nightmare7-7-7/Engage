21f748ae9b5299ac4df61f90c607d89dfe96cc8b
khumanpoudel <khumanpoudel124421@gmail.com>
2026-02-06 07:23:12 +0000

    added validation for reset-password

--- src/validators/user.validators.ts
+++ src/validators/user.validators.ts
@@ -1,27 +1,39 @@
+import { verify } from "crypto";
 import z from "zod"
 
 const msgs = {
-    json_err:'please provide valid details in JSON format',
-    empty_name:"fullname shouldnt be empty",
-    short_name:'fullname must be at least 3 characters long',
-    empty_username:"username shouldnt be empty",
-    short_username:'username must be at least 3 characters long',
-    empty_email:'email shouldnt be empty',
-    invalid_email:'please provide a valid email address',
-    empty_password:"password shouldnt be empty",
-    short_password:'password must be at least 6 characters long',
+    json_err: 'please provide valid details in JSON format',
+    empty_name: "fullname shouldnt be empty",
+    short_name: 'fullname must be at least 3 characters long',
+    empty_username: "username shouldnt be empty",
+    short_username: 'username must be at least 3 characters long',
+    empty_email: 'email shouldnt be empty',
+    invalid_email: 'please provide a valid email address',
+    empty_password: "password shouldnt be empty",
+    short_password: 'password must be at least 6 characters long',
+    empty_verfy_code: "verfyCode shouldn't be empty",
+    short_verfy_code: "verfyCode code must be in 6digits",
+    empty_newPassword: "newPassword shouldnt be empty",
+    short_newPassword: "newPassword must be at least 6 characters long"
 }
 
 export const registerValidator = z.object({
-    fullname: z.string(msgs.empty_name).min(3,msgs.short_name),
-    username: z.string(msgs.empty_username).min(3,msgs.short_username),
+    fullname: z.string(msgs.empty_name).min(3, msgs.short_name),
+    username: z.string(msgs.empty_username).min(3, msgs.short_username),
     email: z.string(msgs.empty_email).email(msgs.invalid_email),
-    password: z.string(msgs.empty_password).min(6,msgs.short_password),
+    password: z.string(msgs.empty_password).min(6, msgs.short_password),
     profile_picture: z.string().optional()
-},msgs.json_err);
+}, msgs.json_err);
 
 
 export const loginValidator = z.object({
     email: z.string(msgs.empty_email).email(msgs.invalid_email),
     password: z.string(msgs.empty_password)
-},msgs.json_err);
+}, msgs.json_err);
+
+
+export const ResetPassValidator = z.object({
+    email: z.string(msgs.empty_email).email(msgs.invalid_email),
+    newPassword: z.string(msgs.empty_newPassword).min(6, msgs.short_newPassword),
+    verfyCode: z.number(msgs.empty_verfy_code).min(6, msgs.short_verfy_code)
+}, msgs.json_err);
