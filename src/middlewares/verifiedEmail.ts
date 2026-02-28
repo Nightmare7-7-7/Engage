8ec764caa2e51839060684c390dfb96828176dc3
khumanpoudel <khumanpoudel124421@gmail.com>
2026-02-28 19:05:35 +0000

    feat: add VerifiedEmail middleware to enforce email verification for feature access

---
+++ src/middlewares/verifiedEmail.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { GotErr } from "../utils/error";
+
+
+interface IUser {
+    id: number,
+    fullname: string,
+    username: string,
+    email: string,
+    is_admin: boolean,
+    email_verified: boolean,
+    iat: number,
+    exp: number
+}
+
+export const VerifiedEmail = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = req.user as IUser
+
+        if (!user.email_verified) {
+            throw new GotErr(403, "you have to verify your email to access this feature")
+        }
+        next()
+    } catch (err: any) {
+        if (err instanceof GotErr) {
+            return res.status(err.code).json({
+                success: false,
+                message: err.message
+            });
+        }
+
+        return res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+}
